5f125b9e0a78d23318425b2040c80dcaacfe0c38
kristijandraca <kristijandraca@gmail.com>
2017-05-17 22:26:33 +0200

    added colors for balance

--- app/src/main/java/com/kristijandraca/databindingexample/DetailsActivity.java
+++ app/src/main/java/com/kristijandraca/databindingexample/DetailsActivity.java
@@ -2,15 +2,23 @@ package com.kristijandraca.databindingexample;
 
 import android.content.Context;
 import android.content.Intent;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 import com.kristijandraca.databindingexample.models.User;
 
 public class DetailsActivity extends AppCompatActivity {
     private static final String ARG_USER = "arg_user";
+
+    @NonNull
+    public static Intent getDetailsIntent(Context context, User user) {
+        Intent intent = new Intent(context, DetailsActivity.class);
+        intent.putExtra(ARG_USER, user);
+        return intent;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,12 +34,6 @@ public class DetailsActivity extends AppCompatActivity {
         tvName.setText(String.format(getString(R.string.format_name_with_age), user.getFirstName(), user.getLastName(), user.getAge()));
         tvType.setText(user.isPremium() ? getString(R.string.type_premium) : getString(R.string.type_regular));
         tvBalance.setText(String.format(getString(R.string.format_balance), user.getBalance()));
-    }
-
-    @NonNull
-    public static Intent getDetailsIntent(Context context, User user) {
-        Intent intent = new Intent(context, DetailsActivity.class);
-        intent.putExtra(ARG_USER, user);
-        return intent;
+        tvBalance.setTextColor(ContextCompat.getColor(this, user.getBalance() > 0 ? R.color.balance_positive : R.color.balance_negative));
     }
 }
